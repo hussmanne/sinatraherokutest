fc6abf4012c6e9c32c62f06adbdda86d9693a1d8
hussmanne <hussman@gmail.com>
2013-02-05 23:12:10 -0700

    blog framework

--- hello.rb
+++ hello.rb
@@ -9,6 +9,28 @@ get '/about' do
 	"This is an about page. Yeah, it's me"
 end
 
+blog = [ 	
+					{
+					  :title => "this is a title",
+						:body => "this is the body"
+					},
+ 					{ 
+ 						:title => "my next title",
+ 					  :body => "this is another body"
+ 					},
+  				{ 
+  					:title => "funny things happen",
+  				  :body => "this is a funny story"
+  				}
+  			]
+
 get '/posts/:slug' do |slug|
-	"This is post " + slug.to_s
+	index = Integer(slug, 10)
+	entry = blog[index]
+	title = entry.fetch(:title)
+	body = entry.fetch(:body)
+	title.to_s + "<br/>" + body.to_s
 end
+
+#blocks and hashes in ruby
+#sinatra templates for blogs, how to return more than a string
